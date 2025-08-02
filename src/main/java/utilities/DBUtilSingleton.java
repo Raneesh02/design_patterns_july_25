84fa80b063f04d5876536c663615a2a40a1885cb
raneeshchoudhary <raneeshchoudhary@gmail.com>
2025-08-02 15:28:03 +0530

    singleton with lazy

--- src/main/java/utilities/DBUtilSingleton.java
+++ src/main/java/utilities/DBUtilSingleton.java
@@ -2,21 +2,33 @@ package utilities;
 
 public class DBUtilSingleton {
 
-    private static DBUtilSingleton dbUtilSingleton = new DBUtilSingleton();
+    //Early initialization
+//    private static DBUtilSingleton dbUtilSingleton = new DBUtilSingleton();
+//    public static DBUtilSingleton getInstance(){
+//        return dbUtilSingleton;
+//    }
+//Early initialization
+
+    //Lazy initialization
+    private static DBUtilSingleton dbUtilSingleton;
 
     private DBUtilSingleton(){
         System.out.println("DB Util constructor called. connection opened" + this.hashCode());
     }
 
     public static DBUtilSingleton getInstance(){
+        if(dbUtilSingleton==null) {
+            dbUtilSingleton = new DBUtilSingleton();
+        }
         return dbUtilSingleton;
     }
+    //lazy initialized
 
     public void runQuery(String query){
         System.out.println(query+ " has run successfully");
     }
 
-    public void printConfig(){
+    public static void printConfig(){
         System.out.println("db configuration");
     }
 }
